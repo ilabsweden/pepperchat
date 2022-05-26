4bb1a219f1b874ecc27afa1606923bad622c3090
Maurice Lamb <maurice.lamb@his.se>
2022-05-26 09:35:31 +0200

    Moved module_speechrecognition to main directory

--- pepperspeechrecognition/speechrecognition.py
+++ pepperspeechrecognition/speechrecognition.py
@@ -17,9 +17,6 @@ import socket
 
 from raw_to_wav import rawToWav
 
-NAO_IP = "127.0.0.1" # default, for running on Pepper
-NAO_PORT = 9559
-
 from optparse import OptionParser
 import naoqi
 import numpy as np
@@ -55,14 +52,15 @@ class SpeechRecognitionModule(naoqi.ALModule):
     Your callback needs to be a method with two parameter (variable name, value).
     """
 
-    def __init__( self, strModuleName, strNaoIp ):
+    def __init__( self, moduleName, naoIp, naoPort=9559):
         try:
-            naoqi.ALModule.__init__(self, strModuleName )
+            naoqi.ALModule.__init__(self, moduleName )
 
             # is these 2 line necessary? what do they do?
             # just copied them from the examples...
             self.BIND_PYTHON( self.getName(),"callback" )
-            self.strNaoIp = strNaoIp
+            self.naoIp = naoIp
+            self.naoPort = naoPort
 
             # declare event to ALMemory so other modules can subscribe
             self.memory = naoqi.ALProxy("ALMemory")
@@ -135,7 +133,7 @@ class SpeechRecognitionModule(naoqi.ALModule):
 
         self.isStarted = False
 
-        audio = naoqi.ALProxy("ALAudioDevice", self.strNaoIp, NAO_PORT)
+        audio = naoqi.ALProxy("ALAudioDevice", self.naoIp, self.naoPort)
         audio.unsubscribe(self.getName())
 
         print("INF: SpeechRecognitionModule: stopped!")
@@ -449,74 +447,3 @@ class SpeechRecognitionModule(naoqi.ALModule):
         print 'Idle release time: ' + str(self.idleReleaseTime)
         print 'Hold time set to: ' + str(self.holdTime)
 
-
-# SpeechRecognition - end
-
-
-    def getAudioDuration(self):
-        return len(self.audio_data)/48000.0
-
-def main():
-    """ Main entry point
-
-    """
-    parser = OptionParser()
-    parser.add_option("--pip",
-        help="Parent broker port. The IP address or your robot",
-        dest="pip")
-    parser.add_option("--pport",
-        help="Parent broker port. The port NAOqi is listening to",
-        dest="pport",
-        type="int")
-    parser.set_defaults(
-        pip=NAO_IP,
-        pport=NAO_PORT)
-
-    (opts, args_) = parser.parse_args()
-    pip   = opts.pip
-    pport = opts.pport
-
-    # We need this broker to be able to construct
-    # NAOqi modules and subscribe to other modules
-    # The broker must stay alive until the program exists
-    myBroker = naoqi.ALBroker("myBroker",
-       "0.0.0.0",   # listen to anyone
-       0,           # find a free port and use it
-       pip,         # parent broker IP
-       pport)       # parent broker port
-
-    try:
-        p = ALProxy("SpeechRecognition")
-        p.exit()  # kill previous instance, useful for developing ;)
-    except:
-        pass
-
-    # Reinstantiate module
-
-    # Warning: SpeechRecognition must be a global variable
-    # The name given to the constructor must be the name of the
-    # variable
-    global SpeechRecognition
-    SpeechRecognition = SpeechRecognitionModule("SpeechRecognition", pip)
-
-    # uncomment for debug purposes
-    # usually a subscribing client will call start() from ALProxy
-    #SpeechRecognition.start()
-    #SpeechRecognition.startRecording()
-    #SpeechRecognition.calibrate()
-    #SpeechRecognition.enableAutoDetection()
-    #SpeechRecognition.startRecording()
-
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        print
-        print "Interrupted by user, shutting down"
-        myBroker.shutdown()
-        sys.exit(0)
-
-
-
-if __name__ == "__main__":
-    main()
