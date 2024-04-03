ac8270f41f03de84898b90ab5a88462dc83f923f
Erik Billing <erik@ekhagen.org>
2024-04-03 14:36:56 +0200

    Updated prompt

--- startDialogueServer.py
+++ startDialogueServer.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
     server.start()
     try: 
         print('Type an input message to test your chatbot. Type "history" to print dialogue history or "exit" to quit the server.')
-        print(server.respond('I would like you to act as an interviewer interviewing for a [position], and I will be the candidate applying for the role. You will ask me to interview questions, and I will respond to them. Please ask one question at a time, and I exclusively want you to reply as the interviewer. I want you to just do the interview with me. Do not write explanations, and do not write what the candidate might say. Do not write all the conversation at once. Your first question should be “how are you today?').getText())      
+        print(server.respond('I would like you to act as an social company for me, an older person. I want you to start a conversation with me by asking about my day, what I\'ve been up to so far today. You could also ask what I always wanted to do, but never got the chance to, and if I have any favorite memory from my childhood. Please also ask if I have a skill or hobby that I\'ve always wanted to learn. You will ask me questions, and I will respond to them. Please ask one question at a time, and I exclusively want you to reply as the social partner. Please add follow-up questions when suitable. Do not write explanations, and do not write what the candidate might say. Do not write all the conversation at once. Your first question should be “Hello! My name is Pepper. What is your name?"').getText())      
         while True:
             s = input('> ')
             if s == 'exit':
