9a60d36d1895eeded1169537f044e598e143bf18
Erik Billing <erik@ekhagen.org>
2025-11-23 18:59:17 +0100

    subs ok

--- micke/speech_to_text/test_openai3.py
+++ micke/speech_to_text/test_openai3.py
@@ -207,54 +207,60 @@ class Oai:
 import comm
 
 class PepperSpeak:
+    class Worker:
+        def __init__(self, pepper_speak:"PepperSpeak"):
+            self.pepper_speak = pepper_speak
+            self.sentences = []
+            self.unsentenced_text = ""
+            self.robot_talking = False
+            self.last_text_receive_time = 0
+            self.done = False
+            def loop():
+                next_sentence_idx = 0
+                while not self.done:
+                    if self.last_text_receive_time > 0:
+                        sentence_cnt = len(self.sentences)
+                        got_all_text = time.time() - self.last_text_receive_time > .5
+                        if sentence_cnt <= next_sentence_idx:
+                            if got_all_text:
+                                if self.unsentenced_text:
+                                    self.sentences.append(self.unsentenced_text)
+                                    self.unsentenced_text = ""
+                                else:
+                                    self.done = True
+
+                        elif not self.robot_talking:
+                            sentences = self.sentences.copy()
+                            sentence = sentences[next_sentence_idx]
+                            pepper_speak.say_text_sender.send(sentence)
+                            print("say:",sentence)
+                            next_sentence_idx += 1
+                            if pepper_speak.subtitle_server:
+                                pepper_speak.subtitle_server.set_text("".join(sentences[:next_sentence_idx]))
+                    time.sleep(.2)
+            threading.Thread(target=loop, daemon=True).start()
+        
+        def push_text(self, text:str):
+            self.last_text_receive_time = time.time()
+            self.unsentenced_text += text
+            if sentences := subtitles.split_into_sentences(self.unsentenced_text):
+                self.unsentenced_text = self.unsentenced_text.removeprefix("".join(sentences))
+                self.sentences += sentences
+
     def __init__(self, subtitle_server:subtitles.SubtitleServer=None):
         self.say_text_sender = comm.TranscriptSender()
-        self._sentences = []
-        
-        self._unsentenced_text = ""
         self.subtitle_server = subtitle_server
-        self._robot_talking = False
-        self._last_text_receive_time = 0
-        self._thread_lock = threading.Lock()
-        def loop():
-            next_sentence_idx = 0
-            while True:
-                if self._last_text_receive_time > 0:
-                    sentence_cnt = len(self._sentences)
-                    got_all_text = time.time() - self._last_text_receive_time > .5
-                    if sentence_cnt <= next_sentence_idx:
-                        if got_all_text:
-                            if self._unsentenced_text:
-                                self._sentences.append(self._unsentenced_text)
-                                self._unsentenced_text = ""
-                            else:
-                                with self._thread_lock:
-                                    self._unsentenced_text = ""
-                                    self._sentences = []
-                                    next_sentence_idx = 0
-                                    self._last_text_receive_time = 0
+        self.worker = PepperSpeak.Worker(self)
 
-                    elif not self._robot_talking:
-                        sentences = self._sentences.copy()
-                        sentence = sentences[next_sentence_idx]
-                        self.say_text_sender.send(sentence)
-                        print("say:",sentence)
-                        next_sentence_idx += 1
-                        if subtitle_server:
-                            subtitle_server.set_text("".join(sentences[:next_sentence_idx]))
-                time.sleep(.2)
-        threading.Thread(target=loop, daemon=True).start()
-    
     def push_text(self, text:str):
-        self._last_text_receive_time = time.time()
-        self._unsentenced_text += text
-        if sentences := subtitles.split_into_sentences(self._unsentenced_text):
-            self._unsentenced_text = self._unsentenced_text.removeprefix("".join(sentences))
-            self._sentences += sentences
-
+        if self.worker.done:
+            self.worker = PepperSpeak.Worker(self)
+        self.worker.push_text(text)
 
     def on_robot_state_change(self, state:comm.RobotState):
-        self._robot_talking = state.talking
+        self.worker.robot_talking = state.talking
+        if state.head_touched:
+            self.worker.done=True
 
 
 
